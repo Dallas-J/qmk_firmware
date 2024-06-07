3563b89dc99e1d83e618c87876209f8c20e21411
Dallas Johnson <dallasajohnson14@gmail.com>
2024-06-07 14:17:42 -0700

    Use unused key as a shortcut to lock the screen

--- keyboards/sirius/unigo66/keymaps/default/keymap.c
+++ keyboards/sirius/unigo66/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ┣━━━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫  \  ┃            ┃  ]  ┃━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━━━┫
 // ┃  SHFT ┃  Z  ┃  X  ┃  C  ┃  V  ┃  B  ┃     ┃            ┃     ┃  N  ┃  M  ┃  ,  ┃  .  ┃  /  ┃ SHFT  ┃
 // ┗━┳━━━━━╋━━━━━╋━━━━━╋━━━━━┻━━━━━┻━━━━━┻━━━━━┛            ┗━━━━━┻━━━━━┻━━━━━┻━━━━━╋━━━━━╋━━━━━╋━━━━━┳━┛
-//   ┃ BRT-┃ BRT+┃ N/C ┃         ┏━━━━━┳━━━━━┓                ┏━━━━━┳━━━━━┓         ┃MUTE ┃VOL- ┃VOL+ ┃
+//   ┃ BRT-┃ BRT+┃ LOCK┃         ┏━━━━━┳━━━━━┓                ┏━━━━━┳━━━━━┓         ┃MUTE ┃VOL- ┃VOL+ ┃
 //   ┗━━━━━┻━━━━━┻━━━━━┛         ┃ DEL ┃     ┃                ┃     ┃ BSP ┃         ┗━━━━━┻━━━━━┻━━━━━┛
 //                               ┣━━━━━┫ SPC ┃                ┃ ENT ┣━━━━━┫
 //                               ┃ GLR ┃     ┃                ┃     ┃ GLR ┃
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,         KC_LBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,                            KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_BSLS,        KC_RBRC, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_BRID, KC_BRIU, KC_NO,                                                                            KC_MUTE, KC_VOLD, KC_VOLU,
+  KC_BRID, KC_BRIU, LGUI(KC_L),                                                                       KC_MUTE, KC_VOLD, KC_VOLU,
                                             KC_DEL, KC_SPC,         KC_ENT, KC_BSPC,
                                             GUILYR, KC_NO,          KC_NO,  GUILYR
     ),
